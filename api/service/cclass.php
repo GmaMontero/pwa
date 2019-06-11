0af9b36b6ea8d6898adf6b2ef2a564515fd76e4e
fedelissi <45604730+fedelissi@users.noreply.github.com>
2019-06-10 22:28:12 -0300

    Update cclass.php

--- api/service/cclass.php
+++ api/service/cclass.php
@@ -2,12 +2,20 @@
 
 require_once('../model/CClass.php');
 require_once('../model/ApiErrorResponse.php');
+require_once('../model/ApiErrorResponse.php');
+$collection = (new MongoDB\Client)->pwa->cclass;
 
 class ClassService {
 
     public function create($classObj){
         #return var_dump(get_object_vars($classObj));
-        return $classObj;
+        $insertResult = $collection->insertOne($classObj->getAsMongoModel());
+       
+        if ($insertResult->getInsertedCount() == 1) {
+           #Obtener id y devolver instancia 
+        } else {
+            return null;   
+        }
     }
 
     public function update($classObj){
