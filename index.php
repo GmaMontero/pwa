a13952f4928ee5b323f02c6aa7218fba668dcbd6
fedelissi <45604730+fedelissi@users.noreply.github.com>
2019-06-14 17:16:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 #);
 #$collection = $client->pwa->cclass;
 #$m = new MongoDB\Client("mongodb://pwa:Trend123@pwa-web.tk:27017/pwa");
-$manager = new MongoDB\Driver\Manager("mongodb://pwa:Trend123@pwa-web.tk:27017/pwa")
+$manager = new MongoDB\Driver\Manager("mongodb://pwa:Trend123@pwa-web.tk:27017/")
 
 ?>
 
