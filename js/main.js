15bb096a62f23ceef3cd0aa9e288b5465ac0dc30
Federico F <50146779+fragolafede@users.noreply.github.com>
2019-06-15 00:32:47 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -37,9 +37,11 @@ var MAIN = (function ($) {
     }
 
     //JSONs de ejemplo
-    var classes = '[{"id":"PWA","nameSubject":"Programacion","career":"sistemas","capacity":40,"turn":"N","commission":"B"},{"id":"IPW","nameSubject":"Intro Prog. Web","career":"sistemas","capacity":30,"turn":"N","commission":"A"}]';
-    var classrooms = '[{"id":"1","number":1520,"floor":5,"capacity":40},{"id":"2","number":1310,"floor":3,"capacity":20}]';
+    var classes = JSON.parse('[{"id":"PWA","nameSubject":"Programacion","career":"sistemas","capacity":40,"turn":"N","commission":"B"},{"id":"IPW","nameSubject":"Intro Prog. Web","career":"sistemas","capacity":30,"turn":"N","commission":"A"}]');
+    var classrooms = JSON.parse('[{"id":"1","number":1520,"floor":5,"capacity":40},{"id":"2","number":1310,"floor":3,"capacity":20}]');
     
+    //alert(classes[1].id);
+
     var registerEvents = () => {
 
         $(".nav-link").on("click", function(e){
