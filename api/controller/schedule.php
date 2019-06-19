8aeb60310d160dc75558f05af1612dd163540c86
Gonzalo Montero <gma.montero@gmail.com>
2019-06-19 00:30:39 -0300

    Start algorithm

--- api/controller/schedule.php
+++ api/controller/schedule.php
@@ -15,9 +15,42 @@ header("Access-Control-Allow-Methods: GET");
 
 $METHOD = $_SERVER['REQUEST_METHOD'];
 
+function getBestClassRoomForCapacity($classRooms,$capacity){
+    $classRoomCandidate = null;
+    $classRoomCandidateDiff = null;
+
+    foreach ($classRooms as $classRoom){
+        $crCapacity = intval($classRoom["capacity"]);
+        $cCapacity = intval($capacity);
+
+        // Si la capacidad del aula es mayor a la de la cursada y la diferencia es menor a la mejor hasta el momento
+        if($crCapacity >= $cCapacity && ($classRoomCandidateDiff > $crCapacity-$cCapacity || $classRoomCandidateDiff === null)){
+            $classRoomCandidate = $classRoom;
+            $classRoomCandidateDiff = $crCapacity-$cCapacity;
+        }
+    }
+
+    return $classRoomCandidate;
+}
+
 switch ($METHOD) {
     case "GET":
-        echo '{"status":"OK"}';
+        $classService = new ClassService();
+        $classroomService = new ClassroomService();
+        $turns = ["M","T","N"];
+        $classRooms = $classroomService->getAll([]);
+
+        // Reemplazar luego por un for adentro para iterar todos los turnos
+        $classes = $classService->getAll(['turn'=>$turns[1]]);
+
+        foreach ($classes as $class){
+            echo "Cursada: ";
+            echo json_encode($class);
+            echo "\nMejor aula encontrada: ";
+            echo json_encode(getBestClassRoomForCapacity($classRooms, $class["capacity"]));
+            echo "\n\n";
+        }
+
         http_response_code(200);
     break;
     default:
