00d0b9e79d01651810fab8f6a3ccea296d651203
julioignaciootero <45604192+julioignaciootero@users.noreply.github.com>
2019-06-21 15:59:23 -0300

    Botones ABM

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <meta name="description" content="The HTML5 Herald">
     <meta name="author" content="SitePoint">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/main.css?v=1.0">
 </head>
 <body>
