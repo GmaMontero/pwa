e854de95e69da21312483b9bfa21f7d7ebed9e49
Federico F <50146779+fragolafede@users.noreply.github.com>
2019-06-22 11:39:44 -0300

    Update main.js
    Borrado de función innecesaria en main

--- js/main.js
+++ js/main.js
@@ -52,24 +52,7 @@ var MAIN = (function ($) {
             containerSchedule.removeClass("d-none");         
         });
 
-        /**
-         * Función para serializar un formulario a JSON
-         */
-        $.fn.serializeFormJSON = function () {
-            var o = {};
-            var a = this.serializeArray();
-            $.each(a, function () {
-                if (o[this.name]) {
-                    if (!o[this.name].push) {
-                        o[this.name] = [o[this.name]];
-                    }
-                    o[this.name].push(this.value || '');
-                } else {
-                    o[this.name] = this.value || '';
-                }
-            });
-            return o;
-        };
+        
     }
 
     /**
