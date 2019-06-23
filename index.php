fa0eacbdae67b4b5e33a2885caf5356babc9ff8d
Federico F <50146779+fragolafede@users.noreply.github.com>
2019-06-23 13:08:36 -0300

    Update index.php
    Menú fixed

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <body>
 
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <a class="navbar-brand" href="#">PWA</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -37,7 +37,8 @@
         </ul>
     </div>
 </nav>
-
+<br />
+<br />
 <div class="container-fluid">
     <br />
     <div class="row containerModule" id="schedule">
